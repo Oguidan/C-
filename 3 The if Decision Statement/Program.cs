1ed3b1e1a818393ec09f30a278e73854ba616d88
ArchangelMichael <michaeloguidan@gmail.com>
2023-04-20 23:23:31 +0000

    Starting the if decision statement

---
+++ 3 The if Decision Statement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
