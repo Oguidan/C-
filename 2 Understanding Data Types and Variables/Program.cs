7b40b4879aca641fbc641994356bc3f21c0a2fba
ArchangelMichael <michaeloguidan@gmail.com>
2023-04-20 08:39:19 +0000

    finish "understanding data types and variables"

---
+++ 2 Understanding Data Types and Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            // string myLastName;
+            // Console.Write("Type your last name: ");
+            // myLastName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+        }
+    }
+}
