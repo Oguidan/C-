7deec6488eb1b2289f6cc3906ba426c02c86c7b7
ArchangelMichael <michaeloguidan@gmail.com>
2023-04-28 22:11:25 +0000

    End of "The if Decision Statement" module

--- 3 The if Decision Statement/Program.cs
+++ 3 The if Decision Statement/Program.cs
@@ -10,15 +10,44 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1, 2 or 3: ");
             string userValue = Console.ReadLine();
 
+            String message = " ";
+
             if (userValue == "1")
             {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
+                message = "You won a new car!";
             }
+            else if (userValue == "2")
+            {
+                message = "You won a new boat!";
+            }
+            else if (userValue == "3")
+            {
+                message = "You won a new cat";
+            }
+            else
+            {
+                message = "Sorry, we don't understand. ";
+                message += "You Lose";
+            }
+            Console.WriteLine(message);
+            */
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "stand of lint";
+
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+            
+            //Console.WriteLine("You won a {0}.", message);
+            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
         }
     }
 }
