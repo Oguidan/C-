98b1d22d5802a0806a2b4146c429d9c13ec9830d
ArchangelMichael <michaeloguidan@gmail.com>
2023-05-05 11:12:32 +0000

    End of "for Iteration Statement" module

--- 5 for Iteration Statement/Program.cs
+++ 5 for Iteration Statement/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+        }
+    }
+}
