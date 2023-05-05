40cfc841a031d11b394051e3a2e0c602151c84eb
ArchangelMichael <michaeloguidan@gmail.com>
2023-05-05 12:17:22 +0000

    End of the "Understand Arrays" module

--- 6 Understand Arrays/Program.cs
+++ 6 Understand Arrays/Program.cs
@@ -10,6 +10,7 @@ namespace UnderstandingArrays
     {
         static void Main(string[] args)
         {
+            /*
             int number1 = 4;
             int number2 = 8;
             int number3 = 15;
@@ -26,7 +27,49 @@ namespace UnderstandingArrays
             }
             else if (number3 == 16)
             {
-                
+
+            }
+            */
+
+            /*
+            int[] numbers = new int[5];
+
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            //numbers[5] = 42;
+
+            // Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            */
+
+            // int[] numbers = new int[] {4, 8, 15, 16, 23, 42};
+            
+            // string[] names = new string[] {"Eddie", "Alex", "Michael", "David Lee"};
+            /*
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            */
+
+            /*
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            */
+
+            string zig = "You can get what you want out of life " + " if you help enough other people get what they want.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
             }
         }
     }
