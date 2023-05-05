1fd9ccd975dec6157c117619f12db1cc10c69a20
ArchangelMichael <michaeloguidan@gmail.com>
2023-05-05 11:19:54 +0000

    Beginning the module "Understand Arrays"

---
+++ 6 Understand Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 4;
+            int number2 = 8;
+            int number3 = 15;
+            int number4 = 16;
+            int number5 = 23;
+
+            if (number1 == 16)
+            {
+
+            }
+            else if (number2 == 16)
+            {
+
+            }
+            else if (number3 == 16)
+            {
+                
+            }
+        }
+    }
+}
