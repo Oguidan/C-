475ef35e8da967bafd961dfb7f695d24b6527a37
ArchangelMichael <michaeloguidan@gmail.com>
2023-06-14 11:50:27 +0000

    Starting "Working with Strings" module

---
+++ 9 Working with Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myString = "My \"so-called\" life";
+            // string myString = "What if I need a\nnew line?";
+            // string myString = "Go to your c:\\ drive";
+            // string myString = @"Go to you c:\ drive";
+
+            string myString = String.Format("{0} = {1}", "First", "Second");
+
+            Console.WriteLine(myString);
+        }
+    }
+}
