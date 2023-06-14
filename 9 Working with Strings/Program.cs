d56db28ab341b6e0328ff03288a7cd1b424d971b
ArchangelMichael <michaeloguidan@gmail.com>
2023-06-14 17:39:44 +0000

    Finish the module "Working with Strings"

--- 9 Working with Strings/Program.cs
+++ 9 Working with Strings/Program.cs
@@ -15,7 +15,35 @@ namespace WorkingWithStrings
             // string myString = "Go to your c:\\ drive";
             // string myString = @"Go to you c:\ drive";
 
-            string myString = String.Format("{0} = {1}", "First", "Second");
+            // string myString = String.Format("{1} = {0}", "First", "Second");
+
+            // string myString = String.Format("{0:c}", 123.45);
+            // string myString = String.Format("{0:N}", 1234567890);
+            // string myString = String.Format("Percentage: {0:P}", .123);
+            // string myString = String.Format("Phone Number: {0:(###) ###-####}", 123456789012);
+
+            // string myString = " That summer we took threes across the board  ";
+
+            // myString = myString.Substring(6, 14);
+            // myString = myString.ToUpper();
+            // myString = myString.Replace(" ", "--");
+            // myString = myString.Remove(6, 14);
+
+            // myString = String.Format("Length before: {0} -- Length after: {1}", myString.Length, myString.Trim().Length);
+
+            /* string myString = "";
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+            } */
+
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
 
             Console.WriteLine(myString);
         }
