5cddbbf5e6ee090c6cc7f227c868e55bac6ea9e5
ArchangelMichael <michaeloguidan@gmail.com>
2023-06-15 16:15:25 +0000

    Finish the moule "Working with Dates and Times"

--- 10 Working with Dates and Times/Program.cs
+++ 10 Working with Dates and Times/Program.cs
@@ -14,7 +14,21 @@ namespace DatesAndTimes
             // Console.WriteLine(myValue.ToString());
             // Console.WriteLine(myValue.ToShortDateString());
             // Console.WriteLine(myValue.ToShortTimeString());
-            Console.WriteLine(myValue.ToLongDateString());
+            // Console.WriteLine(myValue.ToLongDateString());
+            // Console.WriteLine(myValue.ToLongTimeString());
+
+            // Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            // Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            // Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            // Console.WriteLine(myValue.Month);
+
+            // DateTime myBirthday = new DateTime(2001, 05, 06);
+            // Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("05/06/2001");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
         }
     }
 }
