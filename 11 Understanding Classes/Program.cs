c3e5c41f941fc89d60b736eeb113a956e193febc
ArchangelMichael <michaeloguidan@gmail.com>
2023-06-27 17:50:21 +0000

    Start the module "Understanding Classes"

---
+++ 11 Understanding Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car myCar = new car();
+        }
+    }
+
+    class car
+    {
+        public string Make {get; set;}
+        public string Model {get; set;}
+        public int Year {get; set;}
+        public string Color {get; set;}
+    }
+}
