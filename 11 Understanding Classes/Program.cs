6e67ba38e088a6d6aae62f3798aa6376fae2de15
ArchangelMichael <michaeloguidan@gmail.com>
2023-06-30 13:12:40 +0000

    finish the module "Understanding Classes"

--- 11 Understanding Classes/Program.cs
+++ 11 Understanding Classes/Program.cs
@@ -10,15 +10,48 @@ namespace SimpleClasses
     {
         static void Main(string[] args)
         {
-            car myCar = new car();
+            Car myCar = new Car();
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+
+            // decimal value = DetermineMarketValue(myCar);
+            // Console.WriteLine("{0:C}", value);
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+
+            // Someday I might look up the car
+            // online websiervice to get more
+            // accurate value
+            return carValue;
         }
     }
 
-    class car
+    class Car
     {
         public string Make {get; set;}
         public string Model {get; set;}
         public int Year {get; set;}
         public string Color {get; set;}
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+            
+            return carValue;
+        }
     }
 }
